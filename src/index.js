3f2667f9299d4490f5ecae98e0c5e754df79676b
pikokr <parangee9706@gmail.com>
2020-12-08 16:15:58 +0900

    feat: evaluate route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 const Discord = require('discord.js')
 const config = require('../config.json')
+const express = require('express')
 
 process.on('uncaughtException', console.error)
 process.on('unhandledRejection', console.error)
+
+
+const app = express()
+
+app.use(express.json())
+
+app.post('/evaluate', (req, res) => {
+    if (!req.body || !req.body.code) return res.end()
+    new Promise(resolve => resolve(eval(req.body.code))).then(result => {
+        res.json({result})
+    }).catch(err => {
+        res.json({err})
+    })
+})
 
 const client = new Discord.Client({
     presence: {
@@ -10,5 +25,9 @@ const client = new Discord.Client({
     }
 })
 
+client.on('ready', () => {
+    console.log('ready')
+})
 
-client.login()
+
+client.login(config.token).then(() => app.listen(config.internal.port, '127.0.0.1'))
