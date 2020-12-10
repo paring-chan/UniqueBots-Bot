c75f9198825673be5cbc8060572d54f8accfafe4
pikokr <parangee9706@gmail.com>
2020-12-10 15:40:51 +0900

    backup

--- src/index.js
+++ src/index.js
@@ -13,15 +13,24 @@ app.use(express.json())
 app.post('/evaluate', (req, res) => {
     if (!req.body || !req.body.code) return res.end()
     new Promise(resolve => resolve(eval(req.body.code))).then(result => {
+        console.log(result)
         res.json({result})
     }).catch(err => {
+        console.log(err)
         res.json({err})
     })
 })
 
 const client = new Discord.Client({
     presence: {
-        status: 'invisible'
+        status: 'online'
+    }
+})
+
+client.on('guildMemberAdd', async member => {
+    if (member.guild.id !== config.guild) return
+    if (member.user.bot) {
+        await member.roles.add(config.role.pending)
     }
 })
 
